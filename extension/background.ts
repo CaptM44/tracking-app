0997c5877024b70b6e77a13e1940952220b2b3cc
Matthew Morgan <mmorgan@sancsoft.com>
2020-09-16 10:02:54 -0400

    bundling attempt

--- extension/background.ts
+++ extension/background.ts
@@ -1,23 +1,33 @@
+import { browser } from 'webextension-polyfill-ts'
 
-chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: '#3aa757' }, function () {
-    console.log("Starting...");
+chrome.runtime.onInstalled.addListener(async function () {
 
-    let i = 1;
-    setInterval(() => {
-      console.log(i++);
 
-    }, 1000)
 
-  });
+  let t = await browser.storage.sync.set({ color: '#3aa757' });
+  console.log("Starting2...");
+
+  let i = 1;
+  setInterval(async () => {
+    i++;
+    await browser.storage.sync.set({ color: `#${i % 10}aa${i % 10}${i % 10}7` });
+    let tt = await browser.storage.sync.get('color');
+    console.log(tt)
+  }, 1000)
+
 
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: 'developer.chrome.com' },
+        pageUrl: { hostContains: 'c' },
       })
       ],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
 });
+
+// function promisify<T, U>(fn: (args: T, cb: (u: U) => void) => any): (args: T) => Promise<U> {
+//   // await new Promise(r => chrome.storage.sync.set({ color: '#3aa757' }, r));
+//   return t => new Promise<U>(r => fn(t, r));
+// }
