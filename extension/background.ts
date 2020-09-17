dcc9d64fa1773c14ff5c9d21f96afb5f49fe3601
Matthew Morgan <mmorgan@sancsoft.com>
2020-09-17 14:46:43 -0400

    added context menu support

--- extension/background.ts
+++ extension/background.ts
@@ -11,4 +11,16 @@ async function init() {
   chrome.runtime.onMessage.addListener((msg, sender, send) => {
     if (msg.route == '/index') { send(state) }
   });
+
+
+  chrome.contextMenus.create({
+    title: "Tracker App - add track for '%s'",
+    contexts: ["selection"],
+    id: 'ADD:TRACK'
+  });
+  chrome.contextMenus.onClicked.addListener(info => {
+    if (info.menuItemId != 'ADD:TRACK') { return }
+    alert(info.selectionText);
+  });
+
 }
